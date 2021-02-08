42b5645415ea09e6b834339f0dbcd2e32cc5908b
Akash <akashdwivedi02@gmail.com>
2021-02-08 19:46:12 +0530

    News App

--- app/src/main/java/com/example/newsfresh/MainActivity.kt
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.newsfresh
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.bumptech.glide.request.Request
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 
-class MainActivity : AppCompatActivity(), NewsItemClicked {
+    class MainActivity : AppCompatActivity(), NewsItemClicked {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,13 +22,23 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
         val adapter = NewsListAdapter(items, this)
         recyclerView.adapter = adapter
     }
-        private fun fetchData()
-        val url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=de886701f66a4c23bc635a91e3e014c9"
-        val jsonObjectRequest = JsonObjectRequest(
+        private fun fetchData() {
+            val url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=de886701f66a4c23bc635a91e3e014c9"
+            val jsonObjectRequest = JsonObjectRequest(
+                    Request.Method.Get,
+                    url,
+                    null,
+                    Response.Listener {
+                        val newsJsonArray = it.getJSONArray("articles ")
+                        val.newsArray = ArrayList<News>()
+                          for (i in 0 until newsJsonArray.length()) {
+                    },
+                    Response.ErrorListener {
 
-        )
-
-
+                    }
+            )
+        }
+         MySingleton.getInstance
     }
 
     override fun onItemClicked(item: String) {
